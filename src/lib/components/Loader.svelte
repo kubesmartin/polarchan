70e7844fb5601958cc07c4f884b67a54d1a5f171
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-04-11 15:39:11 +0200

    restyle load

--- src/lib/components/Loader.svelte
+++ src/lib/components/Loader.svelte
@@ -1,6 +1,17 @@
-<div class="loader"></div>
+<div class="wrap">
+	<div class="loader"></div>
+</div>
 
 <style>
+	.wrap {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		box-sizing: border-box;
+		width: 100%;
+		padding: 20px;
+		background: var(--c-brand);
+	}
 	.loader {
 		width: fit-content;
 		font-weight: bold;
@@ -10,16 +21,16 @@
 	}
 	.loader::before {
 		content: 'Loading...';
-		color: var(--c-brand);
+		color: var(--c-white);
 		text-shadow:
-			0 0 0 var(--c-brand),
-			10ch 0 0 var(--c-white),
-			20ch 0 0 var(--c-brand);
+			0 0 0 var(--c-white),
+			10ch 0 0 var(--c-brand),
+			20ch 0 0 var(--c-white);
 		background: linear-gradient(
 				90deg,
-				var(--c-brand) 0 calc(100% / 3),
-				var(--c-brand) 0 calc(2 * 100% / 3),
-				var(--c-brand) 0 0
+				var(--c-white) 0 calc(100% / 3),
+				var(--c-white) 0 calc(2 * 100% / 3),
+				var(--c-white) 0 0
 			)
 			left/300% 100%;
 		animation: l23 2s infinite;
@@ -29,16 +40,16 @@
 		50% {
 			background-position: center;
 			text-shadow:
-				-10ch 0 0 var(--c-brand),
-				0 0 0 var(--c-white),
-				10ch 0 0 var(--c-brand);
+				-10ch 0 0 var(--c-white),
+				0 0 0 var(--c-brand),
+				10ch 0 0 var(--c-white);
 		}
 		100% {
 			background-position: right;
 			text-shadow:
-				-20ch 0 0 var(--c-brand),
-				-10ch 0 0 var(--c-white),
-				0 0 0 var(--c-brand);
+				-20ch 0 0 var(--c-white),
+				-10ch 0 0 var(--c-brand),
+				0 0 0 var(--c-white);
 		}
 	}
 </style>
