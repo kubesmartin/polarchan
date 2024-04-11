a9fd8640419118a583804c00bb083fadb3e73a2c
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-04-11 15:29:27 +0200

    Add loader

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,44 @@
+<div class="loader"></div>
+
+<style>
+	.loader {
+		width: fit-content;
+		font-weight: bold;
+		font-family: monospace;
+		font-size: 30px;
+		overflow: hidden;
+	}
+	.loader::before {
+		content: 'Loading...';
+		color: var(--c-brand);
+		text-shadow:
+			0 0 0 var(--c-brand),
+			10ch 0 0 var(--c-white),
+			20ch 0 0 var(--c-brand);
+		background: linear-gradient(
+				90deg,
+				var(--c-brand) 0 calc(100% / 3),
+				var(--c-brand) 0 calc(2 * 100% / 3),
+				var(--c-brand) 0 0
+			)
+			left/300% 100%;
+		animation: l23 2s infinite;
+	}
+
+	@keyframes l23 {
+		50% {
+			background-position: center;
+			text-shadow:
+				-10ch 0 0 var(--c-brand),
+				0 0 0 var(--c-white),
+				10ch 0 0 var(--c-brand);
+		}
+		100% {
+			background-position: right;
+			text-shadow:
+				-20ch 0 0 var(--c-brand),
+				-10ch 0 0 var(--c-white),
+				0 0 0 var(--c-brand);
+		}
+	}
+</style>
